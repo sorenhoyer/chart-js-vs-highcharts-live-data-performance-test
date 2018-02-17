ab20f80f769645d78c2c56600e619a3c3994db35
sorenhoyer <sorenhoyer84@gmail.com>
2018-02-17 09:38:56 +0100

    Chart.js example

---
+++ src/components/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import {observer} from "mobx-react";
+import './App.css';
+//import ChartContainer from './HighchartChart/ChartContainer';
+import ChartContainer from './ChartContainer';
+
+@observer
+class App extends Component {
+  render() {
+    const plotOptions = {
+      series: {
+        turboThreshold: 10000,
+        marker: {
+          enabled: false
+        }
+      }
+    };
+    
+    return (
+      <ChartContainer 
+        sensors={this.props.store.sensors}
+        plotOptions={plotOptions}
+      />
+    );
+  }
+}
+
+export default App;
